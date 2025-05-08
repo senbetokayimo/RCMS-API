d5ac73180a1bea3bd42b074f95f486f7e47b1a66
senbeto <senbeto22@gmail.com>
2025-05-08 09:56:49 +0300

    cors api permission

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const rummersRouter = require("./routes/rumorsRoute");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose"); //including mongoose
+
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(" you are on the home page");
@@ -12,7 +15,6 @@ mongoose
   .connect("mongodb://localhost:27017/rumourDB")
   .then(() => console.log("database connected successfully"))
   .catch((err) => console.log(err));
-
 
 app.get("/users", (req, res) => {
   res.json([
@@ -29,7 +31,7 @@ app.get("/users", (req, res) => {
   ]);
 });
 
-app.use("/rumors", rummersRouter)
+app.use("/rumors", rummersRouter);
 
 app.listen(5000, () => {
   console.log("The servier is running on port 5000");
